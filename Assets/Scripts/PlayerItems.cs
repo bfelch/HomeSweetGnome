71988cadcce1bd8dce544b0e4eef042a56870877
Torikkiji <glodowskie4641@my.uwstout.edu>
2014-09-29 16:59:27 -0500

    Use Items
    Restructured PlayerItem code
    Added UseItem
    Added ShedDoor

--- Assets/Scripts/PlayerItems.cs
+++ Assets/Scripts/PlayerItems.cs
@@ -1,22 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerItems : MonoBehaviour {
+public class PlayerItems : MonoBehaviour
+{
+    public static ArrayList pickUp_names = new ArrayList();  //Stores items that can be picked up
+    public static ArrayList pickUp_values = new ArrayList();  //Stores value for whether items have been picked up
 
-    public static ArrayList pickUp_names = new ArrayList();
-    public static ArrayList pickUp_values = new ArrayList();
+    public static ArrayList useable_names = new ArrayList(); //Stores items that can be interacted with
+    public static ArrayList useable_values = new ArrayList(); //Stores value for whether items have been interacted with
 
-    private bool canHover = false;
+    private bool canHover = false; //Show the item name being look at?
+    private GameObject activeTarget; //The item being looked at
 
     void Start()
     {
-        pickUp_names.Add("ShedKey");
-        pickUp_values.Add(false);
+        pickUp_names.Add("ShedKey"); //Items that can be picked up
+        pickUp_values.Add(false); //Has the item been picked up?
+
+        useable_names.Add("ShedDoor"); //Items that can be interacted with
+        useable_values.Add(false); //Has the item been interacted with?
     }
 
     void Update()
     {
-        PickUp();
+        itemAction();
     }
 
     void OnGUI()
@@ -24,40 +31,77 @@ public class PlayerItems : MonoBehaviour {
         GUI.color = Color.white;
         GUI.backgroundColor = Color.white;
         if (canHover)
-            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 100, 30), "Shed Key");
+        {
+            //Display item name
+            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 100, 30), activeTarget.name);
+        }
     }
 
-    void PickUp()
+    void itemAction()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, fwd, out hit))
         {
-            if (hit.distance <= 5.0 && hit.collider.gameObject.tag == "PickUp")
+            activeTarget = hit.collider.gameObject; //Store item being looked at
+
+            //Is the item close and a pick up?
+            if (hit.distance <= 5.0 && activeTarget.tag == "PickUp")
             {
-                canHover = true;
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    Destroy(hit.collider.gameObject);
-                    for (int j = 0; j < pickUp_names.Count; j++)
-                    {
-                        if (pickUp_names.ToArray()[j].Equals(hit.collider.gameObject.name))
-                        {
-                            pickUp_values[j] = true;
-                            Debug.Log(pickUp_values[j]);
-                        }
-                    }
-                }
+                PickUp(); //Pick it up
+            }
+            //Is the item close and useable?
+            else if (hit.distance <= 5.0 && activeTarget.tag == "Useable")
+            {
+                UseItem(); //Use it
             }
             else
             {
-                canHover = false;
+                canHover = false; //Hide item name
             }
         }
-
     }
 
+    void PickUp()
+    {
+        canHover = true; //Display item name
 
+        //Pressing the E (Interact) key?
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(activeTarget); //Remove item
 
+            //Find index of item being looked at
+            for (int j = 0; j < pickUp_names.Count; j++)
+            {
+                if (pickUp_names.ToArray()[j].Equals(activeTarget.name))
+                {
+                    pickUp_values[j] = true; //Item is picked up
+                }
+            }
+        }
+    }
+
+    void UseItem()
+    {
+        canHover = true; //Display item name
+
+        //Pressing the E (Interact) key?
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Find index of item being looked at
+            for (int j = 0; j < useable_names.Count; j++)
+            {
+                if (useable_names.ToArray()[j].Equals(activeTarget.name))
+                {
+                    if ((bool)pickUp_values.ToArray()[j] == true)
+                    {
+                        Destroy(activeTarget); //Remove item
+                        useable_values[j] = true; //Item was interacted with
+                    }
+                }
+            }
+        }
+    }
 }
