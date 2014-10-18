ffee711f5268b2fa8ee5ca4375698043db5debe2
redm33 <marlee.redmann@gmail.com>
2014-10-18 01:30:34 -0500

    Box Modeled Upstairs Shed

---
+++ Assets/Save and Load/Game.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Game : MonoBehaviour {
+
+    private GameObject[] enemies;
+    private GameObject player;
+    private GameObject[] pickUps;
+    private GameObject[] useable;
+    private GameObject[] structures;
+    public static Game current;
+	// Use this for initialization
+	void Start () {
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+        player = GameObject.FindGameObjectWithTag("player");
+        pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+        useable = GameObject.FindGameObjectsWithTag("Useable");
+        structures = GameObject.FindGameObjectsWithTag("Structure");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
