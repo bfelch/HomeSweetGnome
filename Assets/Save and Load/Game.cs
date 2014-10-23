7b05c240f1b21f7bc81755b0950aa84a7a4e1e37
redm33 <marlee.redmann@gmail.com>
2014-10-22 23:41:00 -0500

    Saving and Loading Started
    When you go through the doorway of shed, game saves. You can load by
    passed through panel behind shed. Have not tested closing game
    completely.

--- Assets/Save and Load/Game.cs
+++ Assets/Save and Load/Game.cs
@@ -2,25 +2,98 @@
 using System.Collections;
 
 [System.Serializable]
-public class Game : MonoBehaviour {
+public class Game{
 
-    private GameObject[] enemies;
-    private GameObject player;
-    private GameObject[] pickUps;
-    private GameObject[] useable;
-    private GameObject[] structures;
-    public static Game current;
-	// Use this for initialization
-	void Start () {
-        enemies = GameObject.FindGameObjectsWithTag("enemy");
-        player = GameObject.FindGameObjectWithTag("player");
-        pickUps = GameObject.FindGameObjectsWithTag("PickUp");
-        useable = GameObject.FindGameObjectsWithTag("Useable");
-        structures = GameObject.FindGameObjectsWithTag("Structure");
+
+    [SerializeField]
+    public float[,] gnomeLocations;
+    [SerializeField]
+    public float[,] gnomeRotations;
+
+    [SerializeField]
+    public float[] playerLocation;
+    [SerializeField]
+    public float[] playerRotation;
+    [SerializeField]
+    public float playerHealth;
+
+    [SerializeField]
+    public ArrayList pickUp_names;
+    [SerializeField]
+    public ArrayList pickUp_values;
+    [SerializeField]
+    public ArrayList useable_names;
+    [SerializeField]
+    public ArrayList useable_values;
+
+    void findObjects () {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject[] pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+        GameObject[] useable = GameObject.FindGameObjectsWithTag("Useable");
+        GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+
+        playerLocation = new float[3];
+        playerRotation = new float[3];
+
+        //gnomeLocations = new float[enemies.Length][];
+        //gnomeRotations = new float[enemies.Length][];
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //public void saveGameValues()
+    //{
+    //    GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    //    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    //    GameObject[] pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+    //    GameObject[] useable = GameObject.FindGameObjectsWithTag("Useable");
+    //    GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+
+    //    for(int k = 0; k < enemies.Length; k++)
+    //    {
+    //        //gnomeLocations[k][0] = enemies[k].transform.position.x;
+    //        //gnomeLocations[k][1] = enemies[k].transform.position.y;
+    //        //gnomeLocations[k][2] = enemies[k].transform.position.z;
+
+    //        //gnomeRotations[k][0] = enemies[k].transform.rotation.x;
+    //        //gnomeRotations[k][1] = enemies[k].transform.rotation.y;
+    //        //gnomeRotations[k][2] = enemies[k].transform.rotation.z;
+    //        //gnomeRotations[k][3] = enemies[k].transform.rotation.w;
+    //    }
+    //    playerLocation = new float[3] { player.transform.position.x, player.transform.position.y, player.transform.position.z };
+    //    playerRotation = new float[3]{ player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z};
+    //    playerHealth = player.gameObject.GetComponent<Player>().sanity;
+    //    pickUp_names = player.gameObject.GetComponent<PlayerInteractions>().pickUp_names;
+    //    pickUp_values = player.gameObject.GetComponent<PlayerInteractions>().pickUp_values;
+    //    useable_names = player.gameObject.GetComponent<PlayerInteractions>().useable_names;
+    //    useable_values = player.gameObject.GetComponent<PlayerInteractions>().useable_values;
+
+    //}
+
+    //public void loadGameValues()
+    //{
+    //    GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    //    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    //    GameObject[] pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+    //    GameObject[] useable = GameObject.FindGameObjectsWithTag("Useable");
+    //    GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+        
+    //    for (int k = 0; k < enemies.Length; k++)
+    //    {
+    //        //enemies[k].transform.position = new Vector3(gnomeLocations[k][0], gnomeLocations[k][1], gnomeLocations[k][2]);
+    //        //enemies[k].transform.rotation = new Quaternion(gnomeRotations[k][0], gnomeRotations[k][1], gnomeRotations[k][2], gnomeRotations[k][3]);
+    //    }
+    //    player.transform.position = new Vector3(playerLocation[0], playerLocation[1], playerLocation[2]);
+    //    player.transform.rotation = new Quaternion(playerRotation[0], playerRotation[1], playerRotation[2],0);
+    //    player.gameObject.GetComponent<Player>().sanity = playerHealth;
+    //    player.gameObject.GetComponent<PlayerInteractions>().pickUp_names = pickUp_names;
+    //    Debug.Log(pickUp_names.Count);
+    //    player.gameObject.GetComponent<PlayerInteractions>().pickUp_values = pickUp_values;
+    //    player.gameObject.GetComponent<PlayerInteractions>().useable_names = useable_names;
+    //    player.gameObject.GetComponent<PlayerInteractions>().useable_values = useable_values;
+
+        
+
+    //}
+
+    
 }
--- Assets/Save and Load/SaveLoad.cs
+++ Assets/Save and Load/SaveLoad.cs
@@ -4,30 +4,125 @@ using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-public static class SaveLoad
+public class SaveLoad: MonoBehaviour
 {
 
     public static List<Game> savedGames = new List<Game>();
 
     //it's static so we can call it from anywhere
-    public static void Save()
+    public void Save()
     {
-        SaveLoad.savedGames.Add(Game.current);
         BinaryFormatter bf = new BinaryFormatter();
-        //Application.persistentDataPath is a string, so if you wanted you can put that into debug.log if you want to know where save games are located
-        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd"); //you can call it anything you want
-        bf.Serialize(file, SaveLoad.savedGames);
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        Game data = new Game();
+        saveGameValues(data);
+
+        bf.Serialize(file, data);
         file.Close();
     }
 
-    public static void Load()
+    public void Load()
     {
-        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-            SaveLoad.savedGames = (List<Game>)bf.Deserialize(file);
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            
+            Game data = (Game)bf.Deserialize(file);
             file.Close();
+
+            loadGameValues(data);
+            //sceneModification(data);
+        }
+        else
+        {
+            Debug.LogError("Couldn't Load File.");
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.name == "Save")
+        {
+            Debug.LogWarning("Saving...");
+            Save();
+        }
+        else if(gameObject.name == "Load")
+        {
+            Debug.LogWarning("Loading...");
+            Load();
+        }
+    }
+    public void saveGameValues(Game data)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject[] pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+        GameObject[] useable = GameObject.FindGameObjectsWithTag("Useable");
+        GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+
+        data.gnomeLocations = new float[enemies.Length,3];
+        data.gnomeRotations = new float[enemies.Length,4];
+
+        for (int k = 0; k < enemies.Length; k++)
+        {
+            data.gnomeLocations[k,0] = enemies[k].transform.position.x;
+            data.gnomeLocations[k,1] = enemies[k].transform.position.y;
+            data.gnomeLocations[k,2] = enemies[k].transform.position.z;
+
+            data.gnomeRotations[k,0] = enemies[k].transform.rotation.x;
+            data.gnomeRotations[k,1] = enemies[k].transform.rotation.y;
+            data.gnomeRotations[k,2] = enemies[k].transform.rotation.z;
+            data.gnomeRotations[k,3] = enemies[k].transform.rotation.w;
+        }
+        data.playerLocation = new float[3] { player.transform.position.x, player.transform.position.y, player.transform.position.z };
+        data.playerRotation = new float[3] { player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z };
+        data.playerHealth = player.gameObject.GetComponent<Player>().sanity;
+        data.pickUp_names = player.gameObject.GetComponent<PlayerInteractions>().pickUp_names;
+        data.pickUp_values = player.gameObject.GetComponent<PlayerInteractions>().pickUp_values;
+        data.useable_names = player.gameObject.GetComponent<PlayerInteractions>().useable_names;
+        data.useable_values = player.gameObject.GetComponent<PlayerInteractions>().useable_values;
+
+    }
+
+    public void loadGameValues(Game data)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject[] pickUps = GameObject.FindGameObjectsWithTag("PickUp");
+        GameObject[] useable = GameObject.FindGameObjectsWithTag("Useable");
+        GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+
+        for (int k = 0; k < enemies.Length; k++)
+        {
+            enemies[k].transform.position = new Vector3(data.gnomeLocations[k,0], data.gnomeLocations[k,1], data.gnomeLocations[k,2]);
+            enemies[k].transform.rotation = new Quaternion(data.gnomeRotations[k,0], data.gnomeRotations[k,1], data.gnomeRotations[k,2], data.gnomeRotations[k,3]);
+        }
+        player.transform.position = new Vector3(data.playerLocation[0], data.playerLocation[1], data.playerLocation[2]);
+        player.transform.rotation = new Quaternion(data.playerRotation[0], data.playerRotation[1], data.playerRotation[2], 0);
+        player.gameObject.GetComponent<Player>().sanity = data.playerHealth;
+        player.gameObject.GetComponent<PlayerInteractions>().pickUp_names = data.pickUp_names;
+        player.gameObject.GetComponent<PlayerInteractions>().pickUp_values = data.pickUp_values;
+        player.gameObject.GetComponent<PlayerInteractions>().useable_names = data.useable_names;
+        player.gameObject.GetComponent<PlayerInteractions>().useable_values = data.useable_values;
+
+        for (int i = 0; i < data.pickUp_names.ToArray().Length; i++)
+        {
+            if ((bool)data.pickUp_values.ToArray()[i])
+            {
+                Destroy(GameObject.Find(data.pickUp_names.ToArray()[i].ToString()));
+            }
+        }
+
+        for (int i = 0; i < data.useable_values.ToArray().Length; i++)
+        {
+            if ((bool)data.useable_values.ToArray()[i] && data.useable_names.ToArray()[i].ToString().Contains("Door"))
+            {
+                GameObject.Find(data.useable_names.ToArray()[i].ToString()).tag = "Door";
+            }
+        }
+
+    }
+
 }
