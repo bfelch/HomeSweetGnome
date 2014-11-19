1e4fb14cf4fabae067f2ddccf3b68d1eca98b595
Torikkiji <glodowskie4641@my.uwstout.edu>
2014-11-19 16:23:06 -0600

    Gate Sounds/Animation

---
+++ Assets/EventScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventScript : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void Play2DAudio(AudioClip ac)
+    {
+        audio.clip= ac;
+        audio.Play();
+    }
+
+    void Play3DAudio(AudioClip ac)
+    {
+        audio.clip = ac;
+        AudioSource.PlayClipAtPoint(ac, transform.position);
+    }
+}
