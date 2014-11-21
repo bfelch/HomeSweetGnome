ead7d68d23f95beda2451416918247788ad3c405
redm33 <marlee.redmann@gmail.com>
2014-11-21 14:20:43 -0600

    Enable/Disable Ojbects

---
+++ Assets/WorldObjects/Entities/Player/LoadUnload.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadUnload : MonoBehaviour {
+
+    private GameObject[] inHedge;
+    private GameObject[] inTunnels;
+    private GameObject[] inShed;
+	// Use this for initialization
+	void Start () {
+	    inHedge = new GameObject[]{GameObject.Find("Dock"), GameObject.Find("EstateWall"), GameObject.Find("Greenhouse"), GameObject.Find("shedModel"), GameObject.Find("Mansion")};
+	    inTunnels = new GameObject[] {GameObject.Find("EstateWall"), GameObject.Find("shedModel"), GameObject.Find("Greenhouse"), GameObject.Find("Hedgemaze"), GameObject.Find("Mansion")};
+        inShed = new GameObject[] {GameObject.Find("Greenhouse"), GameObject.Find("Hedgemaze"), GameObject.Find("Mansion"), GameObject.Find("Dock")};
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.name == "HedgeUnload")
+        {
+            for (int i = 0; i < inHedge.Length; i++)
+            {
+                inHedge[i].SetActive(false);
+            }
+        }
+
+        if(col.name == "HedgeLoad")
+        {
+            for (int i = 0; i < inHedge.Length; i++)
+            {
+                inHedge[i].SetActive(true);
+            }
+        }
+
+        if (col.name == "TunnelUnload")
+        {
+            for (int i = 0; i < inTunnels.Length; i++)
+            {
+                inTunnels[i].SetActive(false);
+            }
+        }
+
+        if (col.name == "TunnelLoad")
+        {
+            for (int i = 0; i < inTunnels.Length; i++)
+            {
+                inTunnels[i].SetActive(true);
+            }
+        }
+
+        if (col.name == "ShedUnload")
+        {
+            for (int i = 0; i < inShed.Length; i++)
+            {
+                inShed[i].SetActive(false);
+            }
+        }
+
+        if (col.name == "ShedLoad")
+        {
+            for (int i = 0; i < inShed.Length; i++)
+            {
+                inShed[i].SetActive(true);
+            }
+        }
+
+    }
+}
