61087b935991f82d6fc265fad88d6e3cf0b47b12
Torikkiji <glodowskie4641@my.uwstout.edu>
2015-01-29 04:10:03 -0600

    Glow/Misc.
    Commented 2 scripts
    Adjusted glow for most objects

---
+++ Assets/Heartbeats/scrHeartbeat.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* This script is an adaptable heartbeat sound system
+ * that plays a heartbeat (slower to faster and visa versa) 
+ * based on the time the player is in contact with danger.
+ */
+public class scrHeartbeat : MonoBehaviour 
+{
+	private GUIDamage guiDamageScript; //GUIDamage Script
+	private float contactCounter; //How fast is the heartbeat?
+	private float waitGap; //How long to wait between each heartbeat
+	private bool enableBeat = false; //Play the heartbeat?
+	private bool beating = false; //Play one beat at a time
+	private bool deathSleep = false;
+	private bool deathFall = false;
+	private AudioSource beat1; //Slowest heartbeat
+	private AudioSource beat2; 
+	private AudioSource beat3;
+	private AudioSource beat4;
+	private AudioSource beat5;
+	private AudioSource beat6;
+	private AudioSource beat7;
+	private AudioSource beat8; //Fastest hearbeat
+
+	//Use this for initialization
+	void Start() 
+	{
+		//Get player's GUI damage script
+		guiDamageScript = GameObject.Find("Player").GetComponent<GUIDamage>();
+
+		//Grab all the audio sources on this object
+		AudioSource[] aSources = GetComponents<AudioSource>();
+		//Set all the audio sources
+		beat1 = aSources[0];
+		beat2 = aSources[1];
+		beat3 = aSources[2];
+		beat4 = aSources[3];
+		beat5 = aSources[4];
+		beat6 = aSources[5];
+		beat7 = aSources[6];
+		beat8 = aSources[7];
+	}
+	
+	//Update is called once per frame
+	void Update () 
+	{
+		//Get the death variables from the pllayer
+		deathFall = GameObject.Find("Player").GetComponent<EndGames>().playerFell;
+		deathSleep = GameObject.Find("Player").GetComponent<EndGames>().playerSlept;
+
+		//Is the player still alive?
+		if(!deathFall && !deathSleep)
+		{
+			//Update variable from other script (how long player is in contact with danger)
+			contactCounter = guiDamageScript.damageTimer;
+
+			//Is the player not in contact with danger?
+			if(contactCounter >= 20.0F)
+			{
+				enableBeat = false; //Enable heartbeat
+			}
+			//Is the player in contact with danger?
+			else
+			{
+				enableBeat = true; //Disable hearbeat
+			}
+
+			//Make sure one beat sound is played at a time
+			if(enableBeat && !beating)
+			{
+				beating = true; //Beat sound is being played
+				waitGap = (contactCounter / 20.0F) + 0.5F; //Calculate time between beats based on contact with danger
+				StartCoroutine(HeartTimer(waitGap)); //Heat beat coroutine
+			}
+		}
+	}
+
+	//Called when in contact with danger based on waitGap (time between beats)
+	IEnumerator HeartTimer(float waitTime)
+	{
+		Heart(); //Play the heat beat
+
+		yield return new WaitForSeconds(waitTime); //Wait the time between beats
+
+		beating = false; //Beat sound is done playing
+	}
+
+	//Chooses a beat to play based off contact with danger
+	void Heart()
+	{
+		//Higher contactCounter plays a slower heartbeat
+		if(contactCounter >= 18)
+		{
+			//Play beat1
+			beat1.Play();
+		}
+		else if(contactCounter >= 16)
+		{
+			//Play beat2
+			beat2.Play();
+		}
+		else if(contactCounter >= 14)
+		{
+			//Play beat3
+			beat3.Play();
+		}
+		else if(contactCounter >= 12)
+		{
+			//Play beat4
+			beat4.Play();
+		}
+		else if(contactCounter >= 10)
+		{
+			//Play beat5
+			beat5.Play();
+		}
+		else if(contactCounter >= 8)
+		{
+			//Play beat6
+			beat6.Play();
+		}
+		else if(contactCounter >= 6)
+		{
+			//Play beat7
+			beat7.Play();
+		}
+		else
+		{
+			//Play beat8
+			beat8.Play();
+		}
+	}
+}
---
+++ Assets/scrFlicker.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* This script can be attached to any light to make it flicker.
+ * The game object name must be specified.
+ */
+public class scrFlicker : MonoBehaviour 
+{
+	private float prevFlickerTime = 0.0f; //Used to calculate next light flicker
+	private float nextFlickerTime = 10.0f; //When the light will flicker next
+	private int flicker = 0; //Flicker counter. (flicker 3 times out of 10)
+
+    private int pattern; //Integer to pick a unique flicker pattern
+    private int[][] patterns = {new int[]{1,5,8}, new int[]{2,5,7}, new int[]{3,5,8}}; //Flicker patterns
+
+    public Material matLightOn; //Bright lightbulb material
+    public Material matLightOff; //Transparent lightbulb material
+	
+	//Update is called once per frame
+	void Update () 
+	{
+        checkFlickerTime();
+	}
+
+	//Checks flicker timer
+	void checkFlickerTime()
+	{
+		//Is the current game time past the last flicker time + next flicker time?
+		if(Time.time > prevFlickerTime + nextFlickerTime)
+		{
+			prevFlickerTime = Time.time; //Set previous flicker time to current game time
+			nextFlickerTime = Random.Range(5, 15); //Set next flicker time
+
+		    pattern = Random.Range(0,3); //Pick a flicker pattern
+            InvokeRepeating("Flicker", 1.0F, 0.1F); //Start flicker pattern
+		}
+	}
+
+	//Flickers the light
+	void Flicker()
+	{
+		//Is the current flicker in the current pattern? (Turn light off)
+        if(System.Array.IndexOf(patterns[pattern], flicker) >= 0)
+        {
+			//Is this game object a Lightbulb?
+            if(this.gameObject.name == "LightBulb")
+            {
+                this.light.enabled = false; //Disable light
+                this.renderer.material = matLightOff; //Switch material
+                (GetComponent("Halo") as Behaviour).enabled = false; //Disable halo
+            }
+        }
+		// The current flicker is not in the current pattern. (Turn light on)
+        else
+        {
+			//Is this game object a Lightbulb?
+            if(this.gameObject.name == "LightBulb")
+            {
+                this.light.enabled = true; //Enable light
+                this.renderer.material = matLightOn; //Switch material
+                (GetComponent("Halo") as Behaviour).enabled = true; //Enable halo
+            }
+        }
+
+		flicker++; //Increase flicker count
+
+		//Is the flicker counter at 10? (Reset)
+		if(flicker >= 10)
+		{
+			flicker = 0; //Set flicker counter back to 0
+			CancelInvoke("Flicker"); //Cancel flicker pattern
+		}
+	}
+}
