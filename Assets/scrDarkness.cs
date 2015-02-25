706cf2eaaca12f072eae7ba5bb9f8c4aed34805f
Evan Glodowski <glodowskie4641@my.uwstout.edu>
2015-02-25 14:30:24 -0600

    Update 2.02
    Darkness Event Script

--- Assets/scrDarkness.cs
+++ Assets/scrDarkness.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class scrDarkness : MonoBehaviour 
 {
 	bool eventStarted = false;
+	GameObject[] lights;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		lights = GameObject.FindGameObjectsWithTag("Light");
 	}
 	
 	// Update is called once per frame
@@ -24,6 +25,22 @@ public class scrDarkness : MonoBehaviour
 			eventStarted = true;
 			transform.GetComponent<MeshCollider>().enabled = true;
 			transform.Find("DarknessTrigger").GetComponent<SphereCollider>().enabled = false;
+			TurnOffLights();
 		}
+	}
+
+	void TurnOffLights()
+	{
+		foreach(GameObject light in lights)
+		{
+			light.GetComponent<Light>().enabled = false;
+		}
+
+		weatherScript weather = GameObject.Find ("Weather").GetComponent<weatherScript>();
+		weather.enabled = false;
+
+		RenderSettings.ambientLight = Color.black;
+
+		GameObject.Find ("MainCamera").GetComponent<GlobalFog> ().enabled = false;
 	}
 }
