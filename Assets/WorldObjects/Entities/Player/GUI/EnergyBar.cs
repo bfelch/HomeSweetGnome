9bac0ae283c61cfca0005fe76469245f125bc024
Torikkiji <glodowskie4641@my.uwstout.edu>
2015-03-23 16:29:50 -0500

    Update 2.33

--- Assets/WorldObjects/Entities/Player/GUI/EnergyBar.cs
+++ Assets/WorldObjects/Entities/Player/GUI/EnergyBar.cs
@@ -23,11 +23,15 @@ public class EnergyBar : MonoBehaviour {
 
             //alters color relative to health
             float sanity = player.sanity / player.maxSanity;
-            if (sanity > .6f) {
+
+            if (sanity > .6f) 
+			{
                 sanityMeter.renderer.material.color = Color.green;
-            } else if (sanity > .3f) {
+            } else if (sanity > .3f) 
+			{
                 sanityMeter.renderer.material.color = Color.yellow;
-            } else {
+            } else 
+			{
                 sanityMeter.renderer.material.color = Color.red;
             }
         }
@@ -36,7 +40,8 @@ public class EnergyBar : MonoBehaviour {
     /*
     void OnGUI() {
         //displays remaining energy and max energy
-        if (gui.hovering) {
+        if (gui.hovering) 
+		{
             Rect box = new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, 150, 30);
             GUI.Box(box, "Energy: " + (int)player.sanity + " / " + (int)player.maxSanity);
         }
